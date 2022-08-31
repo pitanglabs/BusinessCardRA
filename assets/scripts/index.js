1bf74faa97a1bb934390bb5f558df2fd242ab1cd
Anne Livia da Fonseca Macedo <alfm@intranet.pitang.com>
2022-08-31 14:38:56 -0300

    code

---
+++ assets/scripts/index.js
@@ -0,0 +1,27 @@
+const btnGithub = document.querySelector("#github-button");
+const btnLinkedin = document.querySelector("#linkedin-button");
+const btnTwitter = document.querySelector("#twitter-button");
+const btnEmail = document.querySelector("#email-button");
+const btnWebsite = document.querySelector("#web-button");
+
+btnGithub.addEventListener('click', () => {
+    console.log('hello');
+    window.open('https://github.com/', '_blank');
+});
+
+btnLinkedin.addEventListener('click', () => {
+    window.open('https://github.com/', '_blank');
+});
+
+btnTwitter.addEventListener('click', () => {
+    window.open('https://github.com/', '_blank');
+});
+
+
+btnEmail.addEventListener('click', () => {
+    window.open('https://github.com/', '_blank');
+});
+
+btnWebsite.addEventListener('click', () => {
+    window.open('https://github.com/', '_blank');
+});
